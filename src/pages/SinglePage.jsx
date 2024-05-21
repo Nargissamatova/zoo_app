c49012b6caf4c45c612e5b907bacb3eb69b7ef25
Nargis <nargissamatova16@gmail.com>
2024-05-22 01:12:29 +0300

    Add text and img for category page

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,14 +1,44 @@
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "../css/App.css";
 
 function SinglePage({ ...rest }) {
-  const category = useParams();
+  const { category, name } = useParams();
+  const categoryItems = rest[category];
+  const data = categoryItems.find((el) => el.name === name);
 
-  const categoryItems = rest[category.category];
+  const [wikiData, setWikiData] = useState("");
 
-  const data = categoryItems.find((el) => el.name === category.name);
+  useEffect(() => {
+    async function fetchWikiData() {
+      try {
+        const response = await fetch(
+          `https://en.wikipedia.org/api/rest_v1/page/summary/${name}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        setWikiData(result.extract);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchWikiData();
+  }, [name]);
+
   return (
     <>
-      <h2>{data.name}</h2>
+      <h2 className="category_title">{data.name}</h2>
+      <div className="category_container">
+        <img
+          className="category_image"
+          src={`https://source.unsplash.com/300x300/?${data.name}`}
+          alt={data.name}
+        />
+        <p className="category_text">{wikiData}</p>
+      </div>
     </>
   );
 }
